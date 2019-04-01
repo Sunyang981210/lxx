54be2da5927ec6ed621ee05515e227b719140b3a
Mac <mac@macs-imac.local>
2019-04-01 10:51:02 +0800

    Initial Commit

---
+++ lxx/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lxx
+//
+//  Created by Mac on 2019/4/1.
+//  Copyright © 2019 YangSun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
