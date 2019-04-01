b853c4e04aa2a4553cba77a3e9aea6f3600f52ac
FJL <niit@niitdeimac.local>
2019-04-01 16:22:16 +0800

    1.代码分类 2.添加主页、我的模块

---
+++ lxx/home/HomeController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeController.swift
+//  lxx
+//
+//  Created by niit on 2019/4/1.
+//  Copyright © 2019 YangSun. All rights reserved.
+//
+
+
+import UIKit
+
+class HomeController: UIViewController {
+    
+    override func viewDidLoad() {
+    
+    }
+
+}
